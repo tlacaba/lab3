fe8e3502e1e1870b368f220bbf8bf8b5eff68cac
tlacaba <timlacaba123@gmail.com>
2022-10-13 12:54:49 -0700

    Changing tests and fixed array reversal methods

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,10 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
+    for(int i = 0; i < (arr.length / 2); i += 1) { // changed "arr.length" to "(arr.length / 2)"
+      int temp = arr[i]; // added line to perform swap
       arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = temp; // added line to perform swap
     }
   }
 
@@ -14,9 +16,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1]; //was assigning newArray to arr, not the other way around
     }
-    return arr;
+    return newArray; //was returning arr, not newArray
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
